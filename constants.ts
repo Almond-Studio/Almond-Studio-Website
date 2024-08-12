2c17ef49a98ca51b7c251ad9818f593e7c1a6399
Shiven Khurana <shivenbk@gmail.com>
2024-08-12 15:10:39 -0400

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -41,11 +41,11 @@ export const TYPED_STRINGS = [
   "I design and develop motion",
 ];
 
-export const EMAIL = "ayush.singh.xda@gmail.com";
+export const EMAIL = "shivenbk@gmail.com";
 
 export const SOCIAL_LINKS = {
   linkedin: "https://www.linkedin.com/in/alphaayush/",
-  github: "https://github.com/ayush013",
+  github: "https://github.com/Almond-Studio",
   medium: "https://alphaayush.medium.com/",
   instagram: "https://www.instagram.com/alphaayush/",
   facebook: "https://www.facebook.com/ayush013",
